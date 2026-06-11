5d73eda8909b29f1067a45a67b10fb7d6c88c4a3
Uami-11 <nirwan.m.9618@gmail.com>
2026-06-11 12:07:27 +0545

    fix: make the animation of the player attack better and relative

--- entities/player/scripts/StateAttack.cs
+++ entities/player/scripts/StateAttack.cs
@@ -2,6 +2,9 @@ using Godot;
 
 public partial class StateAttack : State
 {
+    [Export]
+    public AnimatedSprite2D attackAnim;
+
     [ExportCategory("Other States")]
     [Export]
     public State idle;
@@ -13,13 +16,31 @@ public partial class StateAttack : State
 
     public override void Enter()
     {
+        if (player.sprite.FlipH)
+        {
+            attackAnim.FlipH = true;
+        }
+        else
+        {
+            attackAnim.FlipH = false;
+        }
+        attackAnim.Visible = true;
         player.UpdateAnimation("attack");
+        if (player.AnimDirection() == "up")
+            attackAnim.ShowBehindParent = true;
+        else
+            attackAnim.ShowBehindParent = false;
+
+        attackAnim.Play("attack_" + player.AnimDirection());
         attacking = true;
 
         player.sprite.AnimationFinished += EndAttack;
     }
 
-    public override void Exit() { }
+    public override void Exit()
+    {
+        player.sprite.AnimationFinished -= EndAttack;
+    }
 
     public override State Process(double delta)
     {
@@ -48,5 +69,6 @@ public partial class StateAttack : State
     public void EndAttack()
     {
         attacking = false;
+        attackAnim.Visible = false;
     }
 }
